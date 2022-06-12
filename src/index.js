644598660c506221c81ed029f13fdbc93903bec1
Ted Bataller <ted.jerbybataller@gmail.com>
2022-06-12 17:00:24 +0800

    second commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import ReactDOM from 'react-dom/client';
+import MyProject from './profile-project/Main.js'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<MyProject/>);
