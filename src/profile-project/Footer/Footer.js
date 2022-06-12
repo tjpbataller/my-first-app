ec8b05c3d996d6fed078ac13cd39e785ea62fa5b
Ted Bataller <ted.jerbybataller@gmail.com>
2022-06-12 17:33:54 +0800

    another try

--- src/profile-project/Footer/Footer.js
+++ src/profile-project/Footer/Footer.js
@@ -10,10 +10,10 @@ import './footer-css.css';
 export default function Footer(){
     return(
         <footer>
-            <a href='https://twitter.com/BatallerTed' target='_blank' rel='opener'><img src={TwitterLogo} alt="twitter logo"/></a>
-            <a href='https://www.facebook.com/official.ted.bataller' target='_blank' rel='opener'><img src={FacebookLogo} alt="facebook logo"/></a>
-            <a href='#' target='_blank' rel='opener'><img src={InstagramLogo} alt="instagram logo"/></a>
-            <a href='https://github.com/tjpbataller' target='_blank' rel='opener'><img src={GitHubLogo} alt="github logo"/></a>
+            <a href='https://twitter.com/BatallerTed' target='_blank' rel='noreferrer'><img src={TwitterLogo} alt="twitter logo"/></a>
+            <a href='https://www.facebook.com/official.ted.bataller' target='_blank' rel='noreferrer'><img src={FacebookLogo} alt="facebook logo"/></a>
+            <a href='#' target='_blank' rel='noreferrer'><img src={InstagramLogo} alt="instagram logo"/></a>
+            <a href='https://github.com/tjpbataller' target='_blank' rel='noreferrer'><img src={GitHubLogo} alt="github logo"/></a>
         </footer>
     )
 }
