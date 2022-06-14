7328549e125e669fdae5c923e627f7e3c2a23a3b
Ted Bataller <ted.jerbybataller@gmail.com>
2022-06-14 11:54:28 +0800

    added card

--- src/airbnb-project/MainComponent/MainComponent.js
+++ src/airbnb-project/MainComponent/MainComponent.js
@@ -1,7 +1,22 @@
+import Swim from './swim.png'
+import Star from './star.png';
+
 export default function MainComponent(){
     return (
         <main>
-            
+            <div>
+                <div>
+                <p>SOLD OUT</p>
+                <img src={Swim} alt='swimmer'/>
+                </div>
+                <div>
+                    <p>
+                        <img src={Star} alt='star'/>
+                        <p className='card--rate'>5.0</p>
+                        <p></p>
+                    </p>
+                </div>
+            </div>
         </main>
     )
 }
