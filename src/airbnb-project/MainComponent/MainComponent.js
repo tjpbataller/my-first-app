a1f0597bae53b9b2733e04dc9932a111d0f41639
Ted Bataller <ted.jerbybataller@gmail.com>
2022-06-16 12:38:26 +0800

    test card overflow

--- src/airbnb-project/MainComponent/MainComponent.js
+++ src/airbnb-project/MainComponent/MainComponent.js
@@ -54,6 +54,54 @@ export default function MainComponent(){
                     </div>
                 </div>
             </div>
+            <div className='card'>
+                <div className='card--status'>SOLD OUT</div>
+                <img className='card--photo' src={Swim} alt='card shown'/>
+                <div className="card--details">
+                    <div className="card--rating">
+                    <img className='star--rating' src={Star} alt="star rating"/>
+                    <span className="rate--number">5.0</span>
+                    <span className="total--booking">(6)</span>
+                    <span className="booking--country">USA</span>
+                    </div>
+                    <div className="card--text">Life lessons with Katie Zaferes</div>
+                    <div className="card--price">
+                    <span><b>From $<span className='booking--price'>136</span></b> / person</span>
+                    </div>
+                </div>
+            </div>
+            <div className='card'>
+                <div className='card--status'>SOLD OUT</div>
+                <img className='card--photo' src={Wed} alt='card shown'/>
+                <div className="card--details">
+                    <div className="card--rating">
+                    <img className='star--rating' src={Star} alt="star rating"/>
+                    <span className="rate--number">5.0</span>
+                    <span className="total--booking">(6)</span>
+                    <span className="booking--country">USA</span>
+                    </div>
+                    <div className="card--text">Lean wedding photography</div>
+                    <div className="card--price">
+                    <span><b>From $<span className='booking--price'>136</span></b> / person</span>
+                    </div>
+                </div>
+            </div>
+            <div className='card'>
+                <div className='card--status'>SOLD OUT</div>
+                <img className='card--photo' src={Bike} alt='card shown'/>
+                <div className="card--details">
+                    <div className="card--rating">
+                    <img className='star--rating' src={Star} alt="star rating"/>
+                    <span className="rate--number">5.0</span>
+                    <span className="total--booking">(6)</span>
+                    <span className="booking--country">USA</span>
+                    </div>
+                    <div className="card--text">Group Mountain Biking activities</div>
+                    <div className="card--price">
+                    <span><b>From $<span className='booking--price'>136</span></b> / person</span>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
