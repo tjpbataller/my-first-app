59b743a138d75295d1e9609b2158d31f8cc6972b
Ted Bataller <ted.jerbybataller@gmail.com>
2022-06-16 12:33:42 +0800

    added more cards

--- src/airbnb-project/MainComponent/MainComponent.js
+++ src/airbnb-project/MainComponent/MainComponent.js
@@ -1,23 +1,59 @@
 import Swim from './swim.png'
-import Star from './star.png';
+import Star from './star.png'
+import Wed from './wedding.png'
+import Bike from './bike.png'
 
 export default function MainComponent(){
     return (
-        <div className='card'>
-        <div className='card--status'>SOLD OUT</div>
-        <img className='card--photo' src={Swim} alt='card shown'/>
-        <div className="card--details">
-            <div className="card--rating">
-            <img className='star--rating' src={Star} alt="star rating"/>
-            <span className="rate--number">5.0</span>
-            <span className="total--booking">(6)</span>
-            <span className="booking--country">USA</span>
+        <section className='card--container'>
+            <div className='card'>
+                <div className='card--status'>SOLD OUT</div>
+                <img className='card--photo' src={Swim} alt='card shown'/>
+                <div className="card--details">
+                    <div className="card--rating">
+                    <img className='star--rating' src={Star} alt="star rating"/>
+                    <span className="rate--number">5.0</span>
+                    <span className="total--booking">(6)</span>
+                    <span className="booking--country">USA</span>
+                    </div>
+                    <div className="card--text">Life lessons with Katie Zaferes</div>
+                    <div className="card--price">
+                    <span><b>From $<span className='booking--price'>136</span></b> / person</span>
+                    </div>
+                </div>
             </div>
-            <div className="card--text">Life lessons with Katie Zaferes</div>
-            <div className="card--price">
-            <span><b>From $<span className='booking--price'>136</span></b> / person</span>
+            <div className='card'>
+                <div className='card--status'>SOLD OUT</div>
+                <img className='card--photo' src={Wed} alt='card shown'/>
+                <div className="card--details">
+                    <div className="card--rating">
+                    <img className='star--rating' src={Star} alt="star rating"/>
+                    <span className="rate--number">5.0</span>
+                    <span className="total--booking">(6)</span>
+                    <span className="booking--country">USA</span>
+                    </div>
+                    <div className="card--text">Life lessons with Katie Zaferes</div>
+                    <div className="card--price">
+                    <span><b>From $<span className='booking--price'>136</span></b> / person</span>
+                    </div>
+                </div>
             </div>
-        </div>
-        </div>
+            <div className='card'>
+                <div className='card--status'>SOLD OUT</div>
+                <img className='card--photo' src={Bike} alt='card shown'/>
+                <div className="card--details">
+                    <div className="card--rating">
+                    <img className='star--rating' src={Star} alt="star rating"/>
+                    <span className="rate--number">5.0</span>
+                    <span className="total--booking">(6)</span>
+                    <span className="booking--country">USA</span>
+                    </div>
+                    <div className="card--text">Life lessons with Katie Zaferes</div>
+                    <div className="card--price">
+                    <span><b>From $<span className='booking--price'>136</span></b> / person</span>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
